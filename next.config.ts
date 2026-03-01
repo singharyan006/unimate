c768b23f566cf0dcf5ec39f0b8b1cb004c9a93ed
Aryan Singh <aryanraisingh2006@gmail.com>
2026-03-01 12:00:00 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
